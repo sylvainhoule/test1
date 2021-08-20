86806b259b35f7f2302eafc28124d20a3c4bc937
Serge Hould <houlds@vaniercollege.qc.ca>
2021-08-20 11:08:11 -0400

    dd

--- win32_project2/lab1_main.cpp
+++ win32_project2/lab1_main.cpp
@@ -17,7 +17,7 @@ int main(void) {
 	set_keyboard_rate(500, 600);
 	textprintf_ex(screen, font, 1, 1, makecol(0,255,0),-1, "Hello World!");
 	textprintf_ex(screen, font, 1, 20, makecol(0,0,255), -1,"Press ESCape to quit");
-	while(! key[KEY_ESC]);
+	while(! key[KEY_ESC]); //
 	allegro_exit();
 	return 0;
 }
